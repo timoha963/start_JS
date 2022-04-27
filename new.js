1a71ce62f190697f26167826784cfc80d1a25033
Tima <timoha_dobrovols@mail.ru>
2022-04-27 19:05:14 +0300

    my task 2

--- new.js
+++ new.js
@@ -1,4 +1,5 @@
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+console.log(numberOfFilms);
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,22 +9,25 @@ const personalMovieDB = {
 };
 
 for (let a = 0; a < 2; a++) {
-    const nameFilm = prompt("Один из последних просмотренных фильмов?", ""),
-          rating = prompt("На сколько оцените фильм?", '');
+    let nameFilm = prompt("Один из последних просмотренных фильмов?", "");
+          
     if (!nameFilm || nameFilm.length > 50) {
-        const nameFilm = prompt("Один из последних просмотренных фильмов?", "");
-    };
+        nameFilm = prompt("Один из последних просмотренных фильмов?", "");
+    }
+
+    const rating = prompt("На сколько оцените фильм?", '');
     personalMovieDB.movies[nameFilm] = rating;
-};
+}
 
 if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов')
-    ('Вы классичесий зритель');
+    alert('Просмотрено довольно мало фильмов');
+    
 } else if (10 <= personalMovieDB.count <= 30) {
-    alert('Вы киноман');
+    alert('Вы классичесий зритель');
 }
 else if (30 < personalMovieDB.count) {
     alert('Вы киноман');
+
 } else {
     alert('Произошла ошибка');
 }
