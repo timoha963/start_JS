fca4897dd61a83e6166537999e540f53c172b8df
Tima <timoha_dobrovols@mail.ru>
2022-05-01 16:16:26 +0300

    my task_5 + help

--- project/js/script.js
+++ project/js/script.js
@@ -24,3 +24,40 @@ const movieDB = {
     ]
 };
 
+// Задание 1
+document.getElementsByClassName('promo__adv')[0].remove();
+
+// Задание 2
+const genre = document.getElementsByClassName('promo__genre')[0];
+const div = document.createElement('div');
+div.classList.add('promo__genre');
+div.innerHTML = 'драма';
+genre.replaceWith(div);
+
+// Задание 3
+const promoBg = document.querySelector('.promo__bg');
+promoBg.style.backgroundImage = 'url("img/bg.jpg")';
+
+// Задание 4 и 5
+
+// const list = document.getElementsByClassName('promo__interactive-list');
+// const item = document.getElementsByClassName('promo__interactive-item');
+// const len = Object.assign({}, item);
+// console.log(len.length);
+// for (let i = 0; i < len.length; i++){
+//     console.log(item[i]);
+//     item[i].remove();
+// }
+
+const promoList = document.querySelector('.promo__interactive-list');
+promoList.innerHTML = '';
+
+movieDB.movies.sort();
+movieDB.movies.forEach( (value, index) => {
+    promoList.innerHTML += `
+        <li class="promo__interactive-item">${index + 1}. ${value}
+             <div class="delete"></div>
+        </li>
+    `;
+});
+
